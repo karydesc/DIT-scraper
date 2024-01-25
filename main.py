3dcc569e2c9b16d095cb9dba9e43c046151d8b12
Chris Karydis <karydes.c@icloud.com>
2024-01-25 19:15:32 +0200

    made the main event loop. now i need to add facebook somehow idk

--- main.py
+++ main.py
@@ -10,19 +10,40 @@ def check404(url_check):
     get404 = temp_soup.find("h1")
     return get404.getText() == "Η σελίδα δεν βρέθηκε"
 
+def writeNewID(web_id):
+    f = open("id.txt", "w")
+    f.write(str(web_id))
+    f.close()
+
 
 f = open("id.txt", "r")
 webid = int(f.read())
 f.close()
-URL = "https://www.dit.uoi.gr/news.php?sa=view_new&id=" + str(webid)
-while check404(URL):
-    time.sleep(5)
+
+
+while True:
+    URL = "https://www.dit.uoi.gr/news.php?sa=view_new&id=" + str(webid)
+    if not check404(URL):
+        print("found at "+str(webid))
+        webid += 1
+        writeNewID(webid) #write to file the incremented id for which the loop will continue searching
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, "html.parser")
+        body = soup.find("div", class_="chris")
+        title = soup.find("h1", class_="title")
+        subtitle = soup.find("h2", class_="subtitle")
+        category_element = soup.find('b', string='Κατηγορία:')
+        category_text = category_element.find_next_sibling(string=True).strip()
+
+        print(" Category: ", category_text, "\n", "Title: " + title.getText(), '\n', "Subtitle: ", subtitle, "\n",
+              body.getText(), '\n')
+        continue
+
     print("not found")
-print("found")
-webid += 1
-f = open("id.txt", "w")
-f.write(str(webid))
-f.close()
+    time.sleep(5)
+
+
+
 
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")

5364d6cc224de6fa6c9b21138dfb3ed349bb9f9f
Christos Karydis <karydes.c@icloud.com>
2024-01-25 13:02:21 +0200

    checking for 404 ok

--- main.py
+++ main.py
@@ -1,18 +1,41 @@
+import time
+
 import requests
 from bs4 import BeautifulSoup
 from lxml import html
-URL = "https://www.dit.uoi.gr/news.php?sa=view_new&id=8733"
+import os
+
+
+
+
+
+f = open("id.txt", "r")
+webid = int(f.read())
+f.close()
+
+URL = "https://www.dit.uoi.gr/news.php?sa=view_new&id=" + str(webid)
+
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, "html.parser")
-body = soup.find("div", class_="chris")
+get404 = soup.find("h1")
+status = get404.getText() == "Η σελίδα δεν βρέθηκε"
 
+if status:
+    time.sleep(5)
+    os.system("python3 main.py")
+
+
+webid += 1
+f = open("id.txt", "w")
+f.write(str(webid))
+f.close()
+
+
+body = soup.find("div", class_="chris")
 title = soup.find("h1", class_="title")
 subtitle = soup.find("h2", class_="subtitle")
 category_element = soup.find('b', string='Κατηγορία:')
-if category_element:
-    # Get the next sibling, which contains the desired text
-    category_text = category_element.find_next_sibling(string=True).strip()
+category_text = category_element.find_next_sibling(string=True).strip()
 
 
-print(" Category: ", category_text, "\n", "Title: " + title.getText(), '\n', "Subtitle: ", subtitle, "\n", body.getText(), '\n')
 
